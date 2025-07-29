f3ed28bfcbee30acf8b0af27c2da0aa871189c2c
Weaam Abu Siam <131193601+weaamabusiam@users.noreply.github.com>
2025-07-29 15:04:40 +0300

    intergartion in index file

--- index.js
+++ index.js
@@ -24,8 +24,21 @@ global.md5 = require('md5');
 
 const auth_Mid = require("./middleware/auth_Mid");
 const auth_R = require('./Routers/authRouter');
+const tasks_R = require('./Routers/tasksRouter');
+const categories_R = require('./Routers/categoriesRouter');
 
 app.use('/', auth_R);
+app.use('/', tasks_R);
+app.use('/', categories_R);
+
+// Apply authentication middleware to protected routes
+app.get('/tasks', auth_Mid.isLogged);
+app.get('/tasks/new', auth_Mid.isLogged);
+app.post('/tasks', auth_Mid.isLogged);
+app.post('/tasks/toggle/:id', auth_Mid.isLogged);
+app.get('/categories', auth_Mid.isLogged);
+app.post('/categories', auth_Mid.isLogged);
+app.get('/categories/delete/:id', auth_Mid.isLogged);
 
 app.get('/', (req, res) => {
     const token = req.cookies.authToken;
